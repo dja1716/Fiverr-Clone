37aa0721e0da9e866313f6c07d064ea78f54f297
dja1716 <hdum@uwaterloo.ca>
2023-03-27 17:13:53 +0900

    react-router-dom basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/navbar/footer/Footer";
+import Home from "./pages/home/Home";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
