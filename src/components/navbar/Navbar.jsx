c39ab37cd7fa6c10a8b74fe740b54f5cc69a2bb3
dja1716 <hdum@uwaterloo.ca>
2023-03-27 16:11:33 +0900

    Navbar animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 export default function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
+        {/* <Link to="/"> */}
         <div className="logo">
           <span className="text">fiverr</span>
           <span className="dot">.</span>
         </div>
+        {/* </Link> */}
         <div className="links">
           <span>Fiverr Business</span>
           <span>Explore</span>
@@ -18,6 +34,15 @@ export default function Navbar() {
           <button>Join</button>
         </div>
       </div>
+      <hr />
+      {active && (
+        <>
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
     </div>
   );
 }
